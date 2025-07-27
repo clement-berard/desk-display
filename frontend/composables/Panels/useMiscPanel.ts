2817cf645f3a3401d673f264667a75f0c23c28ea
Clément BERARD <contact@clementberard.com>
2025-07-27 19:43:52 +0200

    up

--- frontend/composables/Panels/useMiscPanel.ts
+++ frontend/composables/Panels/useMiscPanel.ts
@@ -1,11 +1,12 @@
 import { Blocks } from 'lucide-vue-next';
-import { ref, storeToRefs, useGlobalStore } from '#imports';
+import { computed, ref, storeToRefs, useGlobalStore, useWsNodeRedStore } from '#imports';
 import { Page } from '~/core/Domain/UI/Page';
 import { PageItem } from '~/core/Domain/UI/PageItem';
 import { Panel } from '~/core/Domain/UI/Panel';
 
 export function useMiscPanel() {
   const globalStore = useGlobalStore();
+  const { dataWsNodeRed } = storeToRefs(useWsNodeRedStore());
   const { currentDisplayView } = storeToRefs(globalStore);
   const panel = ref<Panel>(new Panel({ id: 'misc_panel', name: 'Misc', emoji: Blocks }));
 
@@ -27,16 +28,25 @@ export function useMiscPanel() {
         title: 'Fan Xiaomi',
         onClick: () => callHaService('fan/toggle', 'fan.xiaomi_smart_fan'),
         backgroundImage: 'unsplash-1582558006297-f996ed8adfec',
+        isBackgroundImageGray: computed(
+          () => !dataWsNodeRed.value?.main_sensors?.all_main_information?.['fan_xiaomi.power'],
+        ),
       },
       {
         title: '3D Power',
         onClick: () => callHaService('homeassistant/toggle', 'switch.prise_3d_printer'),
         backgroundImage: 'http://local-static.home/misc/p1s.jpg',
+        isBackgroundImageGray: computed(
+          () => !dataWsNodeRed.value?.main_sensors?.all_main_information?.['3d_printer.prise'],
+        ),
       },
       {
         title: '3D Light',
         onClick: () => callHaService('light/toggle', 'light.p1s_01p00c542401101_lumiere_de_la_chambre'),
         backgroundImage: 'unsplash-1705475092160-d2b197d2570b',
+        isBackgroundImageGray: computed(
+          () => !dataWsNodeRed.value?.main_sensors?.all_main_information?.['3d_printer.light'],
+        ),
       },
     ].map((r) => new PageItem(r));
 
